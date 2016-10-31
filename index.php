b6927be39b9031af67d8cc50759adc61b8e0cd9f
Kasidis Chaowvasin <59070011@it.kmitl.ac.th>
2016-10-31 15:43:19 +0700

    Delete navright

--- index.php
+++ index.php
@@ -31,10 +31,6 @@
         <li><a href="#">Page 2</a></li>
         <li><a href="#">Page 3</a></li>
       </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-      </ul>
     </div>
   </div>
   </nav>
