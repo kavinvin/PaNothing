2f5c46ec8877c0ccfa8c9535aae8e9baa88ec91a
Kasidis Chaowvasin <59070011@it.kmitl.ac.th>
2016-10-31 15:48:15 +0700

    Delete link 3

--- index.php
+++ index.php
@@ -29,7 +29,6 @@
         <li class="active"><a href="#">Home</a></li>
         <li><a href="#">Page 1</a></li>
         <li><a href="#">Page 2</a></li>
-        <li><a href="#">Page 3</a></li>
       </ul>
     </div>
   </div>
