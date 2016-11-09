a979e57e23741f65722bb995d4686a97eb800bc2
Kasidis Chaowvasin <59070011@it.kmitl.ac.th>
2016-11-09 18:35:00 +0700

    Backend page for Staff Added

---
+++ backend/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/velocity/1.2.3/velocity.min.js"></script>
+  <script src="../bootstrap/js/bootstrap.js"></script>
+  <link rel="stylesheet" href="../bootstrap/css/bootstrap.css"/>
+  <link href="https://fonts.googleapis.com/css?family=Kanit" rel="stylesheet">
+  <link href="backend.css" rel='stylesheet'>
+</head>
+<body>
+    <div class='col-xs-12 fullscreen'><div class="middle-thing center-thing"><h1>PaNothing</h1><br><h4>Management System</h4></div></div>
+</body>
+</html>
